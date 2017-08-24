c43ce2688960e41298c20eec0e461ad29b22905c
ccc <cardoso.ccc@gmail.com>
2017-08-24 18:00:49 -0300

    Update UILocalNotification+APPLocalNotification.m

--- src/ios/UILocalNotification+APPLocalNotification.m
+++ src/ios/UILocalNotification+APPLocalNotification.m
@@ -63,9 +63,6 @@ NSInteger const APPLocalNotificationTypeTriggered = 2;
     self.applicationIconBadgeNumber = options.badgeNumber;
     self.repeatInterval = options.repeatInterval;
     self.alertBody = options.alertBody;
-    if (NSCalendarUnitEra != options.repeatInterval) {
-        self.repeatInterval = options.repeatInterval;
-    }
     self.soundName = options.soundName;
 
     if ([self wasInThePast]) {
